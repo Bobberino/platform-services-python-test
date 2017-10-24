0dceccacb9b8507f838db09ba57dd4e72a7778f4
rluse <robert.luse@envision-energy.com>
2017-10-23 22:24:50 -0500

    Initial cut at urls.

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,5 +1,8 @@
-from handlers.rewards_handler import RewardsHandler
+from handlers.rewards_handler import RewardsHandler, CustomersHandler
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/customers', CustomersHandler),  # Endpoint 3
+    (r'/customers/(?P<email_address>\w+)', CustomersHandler),  # Endpoint 2
+    (r'/customers/(?P<email_address>\w+)/(?P<order_amount>\w+)', CustomersHandler),  # Endpoint 1
 ]
