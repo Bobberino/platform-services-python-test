cc2d9f3a1605e8746f3ba27f8806c8327d35784e
rluse <robert.luse@envision-energy.com>
2017-10-23 22:26:23 -0500

    Initial CustomerHandler

--- source/RewardsService/rewardsservice/handlers/rewards_handler.py
+++ source/RewardsService/rewardsservice/handlers/rewards_handler.py
@@ -13,3 +13,23 @@ class RewardsHandler(tornado.web.RequestHandler):
         db = client["Rewards"]
         rewards = list(db.rewards.find({}, {"_id": 0}))
         self.write(json.dumps(rewards))
+
+
+class CustomersHandler(tornado.web.RequestHandler):
+
+    @coroutine
+    def get(self):
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        customer = list(db.customers.find({}, {"_id": 0}))
+
+        self.write(json.dumps(customer))
+
+    @coroutine
+    def post(self, email_address, order_value):
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        #db.create_collection("customers")
+        db.customers.insert({"email_address": "customer01@gmail.com", "order_amount": "100"})
+
+
